8644feb031d3e0d05fe3e6707f1704d1ad80e39d
Nick Elobsky <echonok@gmail.com>
2024-03-20 21:14:52 +0100

    Implement navigation menu and enhance structure
    Updated the application layout to include a navigation menu, improving the user experience. The structure changes also enhance the compatibility with MongoDB for the "Place" model while introducing an "Offer" model. All place-related data manipulations are now managed by a new "PlacesService". The offer pages were also updated to utilize the new service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenu,
+  IonMenuToggle,
+  IonRouterOutlet,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { business, checkboxOutline, exit } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel, RouterLink, IonMenuToggle],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({ business, checkboxOutline, exit });
+  }
+
+  onLogOut() {
+
+  }
 }
--- src/app/pages/bookings/bookings.page.ts
+++ src/app/pages/bookings/bookings.page.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonMenuButton],
 })
 export class BookingsPage implements OnInit {
 
